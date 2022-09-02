a038209f892f5c9fbd071bea2ccca4b9d6c4a0db
LeoMarqz <leomarqz2020@gmail.com>
2022-09-01 19:59:34 -0600

    mostrar en home las pubs de los que sigo en proceso

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function __invoke()
     {
-        return view('home');
+        //obtener a quienes seguimos
+        //datos de las personas que sigo en devstagram: id
+        // dd(auth()->user()->following->pluck('id')->toArray());
+        $usersId = auth()->user()->following->pluck('id')->toArray();
+        // dd($usersId);
+        $posts = Post::whereIn('user_id', $usersId)->latest()->paginate(20);
+        // dd($posts);
+        return view('home', ['posts'=>$posts]);
     }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,29 @@ Bienvenidos a Home
 @endsection
 
 @section('content')
+
 <div class="max-w-7xl mx-auto">
-    <h1>Home</h1>
+
+    @if ($posts->count())
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:justify-content-center lg:justify-content-center xl:justify-content-center">
+        @foreach ($posts as $post)
+        <a href="{{route('posts.show', ['user'=>$post->user ,'post'=>$post])}}">
+            <img src="{{ asset('uploads') . "/" . $post->image }}" alt="imagen de post {{ $post->title }}">
+            <p>{{$post->user->username}}</p>
+            <p>{{$post->description}}</p>
+        </a>
+            {{-- <p class="text-gray-500 uppercase text-center font-bold p-4 rounded-lg bg-gray-200">{{$post->title}} <-> {{$post->user->username}}</p> --}}
+        @endforeach
+    </div>
+    @else
+        <p class="text-gray-500 uppercase text-center font-bold p-4 rounded-lg bg-gray-200">No hay posts ...</p>
+    @endif
+        {{-- @forelse ($posts as $post)
+            <p class="text-gray-500 uppercase text-center font-bold p-4 rounded-lg bg-gray-200">{{$post->title}} <-> {{$post->user->username}}</p>
+        @empty
+            <p class="text-gray-500 uppercase text-center font-bold p-4 rounded-lg bg-gray-200">No hay posts ...</p>
+        @endforelse --}}
+
 </div>
 
 @endsection
